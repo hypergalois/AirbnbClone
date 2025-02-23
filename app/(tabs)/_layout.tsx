39e5e7dd7051db467f1d8fa8d6ba803e9d0b5a2f
José Delgado <jose.delgado2@live.u-tad.com>
2025-02-23 20:52:56 +0100

    Add tab layout component with screen options for navigation

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,27 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import Colors from "@/constants/Colors";
+
+const Layout = () => {
+	return (
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: Colors.primary,
+				tabBarLabelStyle: {
+					fontFamily: "Montserrat-SemiBold",
+				},
+				tabBarIcon: ({color, size}) => ()
+			}}
+		>
+			<Tabs.Screen
+				name="index"
+				options={{
+					tabBarLabel: "Explore",
+				}}
+			/>
+		</Tabs>
+	);
+};
+
+export default Layout;
