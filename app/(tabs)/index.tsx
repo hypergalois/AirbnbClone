31490e19e9b0e308539bf8e298e9f8d50a34cef2
José Delgado <jose.delgado2@live.u-tad.com>
2025-02-24 05:14:41 +0100

    Add login, booking, and listing screens with navigation links

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Link } from "expo-router";
 
 const index = () => {
 	return (
 		<View>
 			<Text>index</Text>
+			<Link href={"/(modals)/login"}>Login</Link>
+			<Link href={"/(modals)/booking"}>Bookings</Link>
+			<Link href={"/listing/232"}>Listing details</Link>
 		</View>
 	);
 };
