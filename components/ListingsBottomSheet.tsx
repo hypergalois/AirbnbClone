623e30a61b327df5351a85066bbf3745eff42542
hypergalois <jose.delgado2@live.u-tad.com>
2025-02-28 20:41:37 +0100

    Add ExploreHeader, Listings, and ListingsBottomSheet components

---
+++ components/ListingsBottomSheet.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const ListingsBottomSheet = () => {
+  return (
+    <View>
+      <Text>ListingsBottomSheet</Text>
+    </View>
+  );
+};
+
+export default ListingsBottomSheet;
