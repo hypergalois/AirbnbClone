bae0236d6cb8929e4d660c76226552dfc906aa2f
hypergalois <jose.delgado2@live.u-tad.com>
2025-03-02 01:57:08 +0100

    Enhance Listings component with loading state and improved item rendering

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,5 +1,16 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  ListRenderItem,
+} from "react-native";
+import React, { useEffect } from "react";
+import { defaultStyles } from "@/constants/Styles";
+import Animated, { FadeInRight, FadeOutLeft } from "react-native-reanimated";
+import { Ionicons } from "@expo/vector-icons";
+import { Link } from "expo-router";
 
 interface Props {
   listings: Array<any>;
@@ -7,15 +18,90 @@ interface Props {
 }
 
 const Listings = ({ listings, category }: Props) => {
-  React.useEffect(() => {
-    console.log("reload listings", listings.length);
+  const [loading, setLoading] = React.useState(true);
+  const listRef = React.useRef<FlatList | null>(null);
+
+  useEffect(() => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 200);
   }, [category]);
 
+  const renderRow: ListRenderItem<any> = ({ item }) => (
+    <Link href={`/listing/${item.id}`} asChild>
+      <TouchableOpacity>
+        <Animated.View
+          style={styles.listing}
+          entering={FadeInRight}
+          exiting={FadeOutLeft}
+        >
+          <Animated.Image
+            source={{ uri: item.medium_url }}
+            style={styles.image}
+          />
+          <TouchableOpacity
+            style={{ position: "absolute", right: 30, top: 30 }}
+          >
+            <Ionicons name="heart-outline" size={24} color="#000" />
+          </TouchableOpacity>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <Text style={{ fontSize: 16, fontFamily: "Montserrat-SemiBold" }}>
+              {item.name}
+            </Text>
+            <View style={{ flexDirection: "row", gap: 4 }}>
+              <Ionicons name="star" size={16} />
+              <Text style={{ fontFamily: "Montserrat-SemiBold" }}>
+                {item.review_scores_rating / 20}
+              </Text>
+            </View>
+          </View>
+          <Text style={{ fontFamily: "Montserrat" }}>{item.room_type}</Text>
+          <View style={{ flexDirection: "row", gap: 4 }}>
+            <Text style={{ fontFamily: "Montserrat-SemiBold" }}>
+              € {item.price}
+            </Text>
+            <Text style={{ fontFamily: "Montserrat" }}>night</Text>
+          </View>
+        </Animated.View>
+      </TouchableOpacity>
+    </Link>
+  );
+
   return (
-    <View>
-      <Text>Listings</Text>
+    <View style={defaultStyles.container && { marginTop: 10 }}>
+      <FlatList
+        renderItem={renderRow}
+        data={loading ? [] : listings}
+        ref={listRef}
+        ListHeaderComponent={
+          <Text style={styles.info}>{listings.length} homes</Text>
+        }
+      />
     </View>
   );
 };
 
 export default Listings;
+
+const styles = StyleSheet.create({
+  listing: {
+    padding: 16,
+    gap: 10,
+    marginTop: 12,
+  },
+  image: {
+    width: "100%",
+    height: 300,
+    borderRadius: 10,
+  },
+  info: {
+    textAlign: "center",
+    fontFamily: "mon-sb",
+    fontSize: 16,
+    marginTop: 4,
+  },
+});
