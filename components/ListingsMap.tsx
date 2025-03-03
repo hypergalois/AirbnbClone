ab0976ae9e14217f504afadb953061e4ea0be337
hypergalois <jose.delgado2@live.u-tad.com>
2025-03-03 17:57:14 +0100

    Add listings prop to ListingsMap component and define styles for markers and buttons

--- components/ListingsMap.tsx
+++ components/ListingsMap.tsx
@@ -1,7 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const ListingsMap = () => {
+interface Props {
+  listings: any;
+}
+
+const ListingsMap = ({ listings }: Props) => {
   return (
     <View>
       <Text>ListingsMap</Text>
@@ -10,3 +14,44 @@ const ListingsMap = () => {
 };
 
 export default ListingsMap;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  marker: {
+    padding: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    elevation: 5,
+    borderRadius: 12,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    shadowOffset: {
+      width: 1,
+      height: 10,
+    },
+  },
+  markerText: {
+    fontSize: 14,
+    fontFamily: "mon-sb",
+  },
+  locateBtn: {
+    position: "absolute",
+    top: 70,
+    right: 20,
+    backgroundColor: "#fff",
+    padding: 10,
+    borderRadius: 10,
+    elevation: 2,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    shadowOffset: {
+      width: 1,
+      height: 10,
+    },
+  },
+});
