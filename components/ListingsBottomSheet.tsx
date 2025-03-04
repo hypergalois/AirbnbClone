9b6ec35b676ce89c7f409fa6bc7c3f16ad77bdbe
hypergalois <jose.delgado2@live.u-tad.com>
2025-03-04 02:38:05 +0100

    Add styles to ListingsBottomSheet component

--- components/ListingsBottomSheet.tsx
+++ components/ListingsBottomSheet.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 const ListingsBottomSheet = () => {
@@ -10,3 +10,35 @@ const ListingsBottomSheet = () => {
 };
 
 export default ListingsBottomSheet;
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    flex: 1,
+  },
+  absoluteView: {
+    position: "absolute",
+    bottom: 30,
+    width: "100%",
+    alignItems: "center",
+  },
+  btn: {
+    backgroundColor: Colors.dark,
+    padding: 14,
+    height: 50,
+    borderRadius: 30,
+    flexDirection: "row",
+    marginHorizontal: "auto",
+    alignItems: "center",
+  },
+  sheetContainer: {
+    backgroundColor: "#fff",
+    elevation: 4,
+    shadowColor: "#000",
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+  },
+});
