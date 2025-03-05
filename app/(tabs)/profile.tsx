00fe07c109502c94618b985d2dcf90032145a464
hypergalois <jose.delgado2@live.u-tad.com>
2025-03-05 06:01:16 +0100

    Add styles to Profile component for improved layout and design

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,55 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 const Profile = () => {
-	return (
-		<View>
-			<Text>Profile</Text>
-		</View>
-	);
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  );
 };
 
 export default Profile;
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 24,
+  },
+  header: {
+    fontFamily: "mon-b",
+    fontSize: 24,
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 24,
+    borderRadius: 16,
+    marginHorizontal: 24,
+    marginTop: 24,
+    elevation: 2,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowRadius: 6,
+    shadowOffset: {
+      width: 1,
+      height: 2,
+    },
+    alignItems: "center",
+    gap: 14,
+    marginBottom: 24,
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: Colors.grey,
+  },
+  editRow: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 8,
+  },
+});
